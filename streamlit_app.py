8554eb025eb42bba2fb148e9d2995f2c8380e8a6
dretzlforsnowflake <117237886+dretzlforsnowflake@users.noreply.github.com>
2022-11-01 19:37:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.text('🥑 Avacado Toast 🍞')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_select = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
